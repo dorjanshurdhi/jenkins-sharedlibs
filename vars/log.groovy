cf908b8851d6283f598406edc629c43397143b04
Dorjan Shurdhi <shurdhidorian@gmail.com>
2022-02-27 16:14:05 +0100

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -24,7 +24,7 @@ def error(message){
 
 def success(message){
   loadColors()
-  sh """ set +x; echo -e "${BIGreen}[SUCCESS] - ${Blink} $message  ${NC}" """
+  sh """ set +x; echo -e "${BIGreen}[SUCCESS] - ${Blink} $message  ${NC} ${NC}" """
 }
 
 def debug(message){
