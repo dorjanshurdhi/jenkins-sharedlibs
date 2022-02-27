a7992619abb63a5855a4275bdb031ead28539903
Dorjan Shurdhi <shurdhidorian@gmail.com>
2022-02-27 16:26:16 +0100

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,8 +1,12 @@
 def call(name){
   if( name == "Vincenzo"){
-      message="Hey you are ${name} Disummo?"
-      log.success(message)
-  } 
+    timestamps{
+        ansiColor('xterm'){ 
+          message="Hey you are ${name} Disummo?"
+          log.success(message)
+         } 
+      }
+    }
   else {
     timestamps{
       ansiColor('xterm'){
