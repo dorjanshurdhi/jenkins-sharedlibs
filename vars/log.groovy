f86cfaabbea8765620baac9b275e5f8c84a8ed54
Dorjan Shurdhi <shurdhidorian@gmail.com>
2022-02-27 15:50:43 +0100

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,16 +1,14 @@
 def loadColors(){
   
-  NC='\033[0m'       # Text Reset
-
-  # Bold High Intensity
-  BIBlack='\033[1;90m'      # Black
-  BIRed='\033[1;91m'        # Red
-  BIGreen='\033[1;92m'      # Green
-  BIYellow='\033[1;93m'     # Yellow
-  BIBlue='\033[1;94m'       # Blue
-  BIPurple='\033[1;95m'     # Purple
-  BICyan='\033[1;96m'       # Cyan
-  BIWhite='\033[1;97m'      # White
+  NC='\033[0m'       
+  BIBlack='\033[1;90m'
+  BIRed='\033[1;91m'
+  BIGreen='\033[1;92m'
+  BIYellow='\033[1;93m'
+  BIBlue='\033[1;94m'
+  BIPurple='\033[1;95m'
+  BICyan='\033[1;96m'
+  BIWhite='\033[1;97m'
 }
 
 def info(message){
